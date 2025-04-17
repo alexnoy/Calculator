b22a03ffff3ef0837841ca4e67fd8d6d46eb5fd0
alexnoy <alexander.noy123@gmail.com>
2025-04-16 22:26:45 -0400

    Create basic calculator functions
    Added essential calculator functions required to work such as add, subtract etc.

---
+++ script.js
@@ -0,0 +1,15 @@
+const addition = function(a, b) {
+    return a + b;    
+}
+
+const subtraction = function(a, b) {
+    return a - b;
+}
+
+const multiplication = function(a, b) {
+    return a * b;
+}
+
+const division = function(a, b) {
+    return a / b;
+}
